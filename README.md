42a5f5b12f700a5482f043b8f6e4eff352b5b070
bento40 <phantaporntr@gmail.com>
2022-05-26 20:30:14 +0700

    image in readme

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 This is the example code structure to present the idea for Net Zero Carbon Credit Oracle where small sustainability activities that inline with carbon credit calculation is recored in a decentralized and transparent way for future benefit as well as future validation for tradable voluntary carbon credit.
 
+![alt text](https://github.com/bento40/carboncreditoracle/blob/master/schematic.png?raw=true)
+
 ## Installation
 
+There are two ways to run the code. First, use [Remix IDE](https://remix.ethereum.org/) or run using truffle
 Install Node.
 
 ```bash
@@ -24,7 +27,7 @@ truffle develop
 
 ## Usage
 
-The flow of function shall be as follow;
+The smart contract is an example of smart meter to gain carbon credit from energy efficiency (reduce usage and optimize energy) flow of function shall be as follow;
 
 1.
 
--- contracts/Carboncredit.sol
+++ contracts/Carboncredit.sol
@@ -1,84 +1,69 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity 0.8.13;
 pragma experimental ABIEncoderV2;
 
 contract Carboncredit {
+    mapping(string => address) public meter_owner; //mapping meter_api_key with metamask wallet
 
-    //mapping(address => string) public meter;
-
-    mapping(string => address) public meter_owner;
-
+    // Smart Meter Data. Simplify version using only kWH.
     struct SmartMeter {
-        string public_key; 
-        //uint256 tuya_dev_measure_reg;	    //Register parameters of energy metering.
-        //bool    tuya_dev_measure_start;	    //Enable the energy metering feature.
-        int tuya_get_emasure_energy;	    //Get the energy usage during a specified period that is set on the mobile app. Once the meter is read, the usage data will be zeroed out to start a new measuring cycle.       
+        string public_key;
+        int256 tuya_get_emasure_energy; //Get the energy usage during a specified period that is set on the mobile app. Once the meter is read, the usage data will be zeroed out to start a new measuring cycle.
     }
 
-    SmartMeter[] meter_data;    //array of data from that smart meter
+    SmartMeter[] meter_data; //array of data from that smart meter per period
 
-    mapping(address => SmartMeter[])  owner_data;
-    mapping(address => int) owner_carbon;
+    mapping(address => SmartMeter[]) owner_data; //mapping meter data train with address
+    mapping(address => int256) owner_carbon; //mapping carbon credit data with address
 
-    //Link Metamask Wallet with IoT Device 1 user per 1 device (require call back to verify)
+    // Link Metamask Wallet with IoT Device or centralize server data api (require call back to verify in the future)
     function linkDevice(string memory public_key) external {
         meter_owner[public_key] = msg.sender;
     }
-    //get Public_Key for each address 
-    function getAddress(string memory public_key) public view returns(address) {
+
+    // get Public_Key for each address
+    function getAddress(string memory public_key)
+        public
+        view
+        returns (address)
+    {
         return meter_owner[public_key];
     }
-    
-    function fetchIotData(string memory public_key, int energy) external {
-        address address_owner = getAddress(public_key); 
 
+    // fetch data from iot device using oracle
+    function fetchIotData(string memory public_key, int256 energy) external {
+        address address_owner = getAddress(public_key);
         meter_data = owner_data[address_owner];
-
-        //uint256 meter_length = meter_data.length;
- 
-        meter_data.push(SmartMeter(public_key,energy));
-
+        meter_data.push(SmartMeter(public_key, energy));
         owner_data[address_owner] = meter_data;
-
-        //calculate carbon credit from fetch data
-        // if (meter_length != 0){
-        // uint256 energy_before = meter_data[meter_length].tuya_get_emasure_energy;
-        // _calCarbon(address_owner, energy_before, energy);
-        // }
     }
 
-    function getData() public view returns(SmartMeter[] memory){
+    // output smart meter data train
+    function getData() public view returns (SmartMeter[] memory) {
         return owner_data[msg.sender];
     }
 
-    //function _calCarbon(address owner, uint256 carbon_before, uint256 carbon_after ) private {
-    function _calCarbon() public returns(int) {
-    
-        // uint256 carbon_offset = carbon_before - carbon_after ;
-        // if (carbon_offset > 0){
-        //     owner_carbon[owner] = carbon_offset;
-        // }
-        // external
+    // calculate carbon credit from energy efficiency read from smart meter. Simplify version
+    function _calCarbon() public returns (int256) {
         SmartMeter[] memory meter_data1 = owner_data[msg.sender];
         uint256 meter_length = meter_data1.length;
-        int energy_after = meter_data1[meter_length-1].tuya_get_emasure_energy;
-        int energy_before = meter_data1[meter_length-2].tuya_get_emasure_energy;
+        int256 energy_after = meter_data1[meter_length - 1]
+            .tuya_get_emasure_energy;
+        int256 energy_before = meter_data1[meter_length - 2]
+            .tuya_get_emasure_energy;
 
-        int carbon_offset = energy_after - energy_before ;
-        owner_carbon[msg.sender] = owner_carbon[msg.sender]+carbon_offset;
-        return owner_carbon[msg.sender];
-
-        // if (carbon_offset < 0){
-        //     owner_carbon[msg.sender] = owner_carbon[msg.sender]-carbon_offset;
-        // }
-    }
-
-    function getCarbonCredit() public view returns(int){
+        int256 carbon_offset = energy_after - energy_before;
+        owner_carbon[msg.sender] = owner_carbon[msg.sender] + carbon_offset;
         return owner_carbon[msg.sender];
     }
 
-    function redeemCarbonCredit() external{
+    // output carbon credit of user
+    function getCarbonCredit() public view returns (int256) {
+        return owner_carbon[msg.sender];
+    }
+
+    // redeem carbon credit from approval metaverse
+    function redeemCarbonCredit() external {
         owner_carbon[msg.sender] = 0;
     }
 }
